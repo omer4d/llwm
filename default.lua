0039068433b5a7b89d4c51daf22f5a90d754233c
stas <omer4d@gmail.com>
2016-01-30 10:32:29 +0000

    working command tree

--- default.lua
+++ default.lua
@@ -4,6 +4,12 @@ function tail(arr)
    return { select(2, unpack(arr)) }
 end
 
+function countItems(tbl)
+   local count = 0
+   for _ in pairs(tbl) do count = count + 1 end
+   return count
+end
+
 CommandTreeNode = {}
 
 CommandTreeNode.new = function()
@@ -12,73 +18,125 @@ CommandTreeNode.new = function()
    return o
 end
 
+CommandTreeNode.findChild = function(self, ch)
+   for k,v in pairs(self.childSet) do
+      if k[1] == ch[1] and k[2] == ch[2] then return v end
+   end
+   return nil
+end
+
 CommandTreeNode.addCmd = function(self, path, cmd)
    if #path == 0 then self.cmd = cmd return end   
-   local key = path[1][1] .. path[1][2]
+   local child = self:findChild(path[1])
    
-   if not self.childSet[key] then
-      self.childSet[key] = CommandTreeNode.new()
+   if not child then
+      child = CommandTreeNode.new()
+      self.childSet[path[1]] = child
    end
-
-   (self.childSet[key]):addCmd(tail(path), cmd)
+   
+   child:addCmd(tail(path), cmd)
 end
 
 CommandTreeNode.isLeaf = function(self)
-   local count = 0
-   for _ in pairs(self.childSet) do count = count + 1 end
-   return count == 0
+   return countItems(self.childSet) == 0
 end
 
 CommandTreeNode.printPaths = function(self, accum)
    if self:isLeaf() then print(accum) return end
-   for k,v in pairs(self.childSet) do
-      v:printPaths(accum .. " " .. k)
+   for k,_ in pairs(self.childSet) do
+      v:printPaths(accum .. " " .. k[2] .. "(" .. k[1] .. ")")
    end
 end
 
-CommandTreeNode.walk = function(self, ch)
-   local child = self.childSet[ch[1] .. ch[2]]
+CommandTreeTraverser = {}
+
+CommandTreeTraverser.new = function(root, callback)
+   local o = {root = root, callback = callback, currNode = root}
+   setmetatable(o, {__index = CommandTreeTraverser})
+   return o
+end
+
+CommandTreeTraverser.step = function(self, ch)
+   local child = self.currNode:findChild(ch)
    
    if child then
       if child:isLeaf() then
-	 return false, child.cmd
+	 self.currNode = self.root
+	 self.callback(child.cmd)
+	 return false
       else
-	 return true, function (ch) return child:walk(ch) end
+	 self.currNode = child
+	 return true
       end
    else
-      return false, nil
+      self.currNode = self.root
+      self.callback(nil)
+      return false
    end
 end
 
-keyPressed = {}
+function grabRootKeys(root)
+   for k,_ in pairs(root.childSet) do
+      wm.grabKey(k[1], k[2])
+   end
+end
 
-wm.grabKey(64, "q")
-wm.grabKey(64, "t")
-wm.grabKey(64, "g")
+function cmdTreeTravCb(cmd)
+   if cmd then cmd() else print("Unknown command sequence!") end
+end
+
+MOD_KEYS = {
+   Super_L = true,
+   Super_R = true,
+   Control_L = true,
+   Control_R = true,
+   Shift_L = true,
+   Shift_R = true,
+   Alt_L = true,
+   Alt_R = true
+}
+
+cmdTree = CommandTreeNode.new()
+
+cmdTree:addCmd({{64, "q"}}, function()
+      print("Quitting...")
+      wm.quit()
+end)
+
+cmdTree:addCmd({{64, "t"}}, function()
+      os.execute("urxvt")
+end)
+
+cmdTree:addCmd({{64, "x"}, {0, "r"}}, function()
+      print("Reloading!")
+      dofile("default.lua")
+end)
+
+cmdTraverser = CommandTreeTraverser.new(cmdTree, cmdTreeTravCb)
+
+keyPressed = {}
+scheduledKbRelease = false
+wm.releaseAllKeys()
+grabRootKeys(cmdTree)
 
 wm.onKeyDown(function (win, mods, key)
-      keyPressed[key] = true;
-
-      print(key .. mods .. " down")
+      if not keyPressed[key] and not MOD_KEYS[key] then
+	 --print(key .. mods .. " pressed")
+	 
+	 if cmdTraverser:step({mods, key}) then wm.grabKeyboard() else scheduledKbRelease = true end
+      end
       
-      if key == "q" then
-	 wm.quit()
-      end
-
-      if key == "t" then
-	 print("Grabbing keyboard...")
-	 wm.grabKeyboard()
-      end
-
-      if key == "g" then
-	 print("Releasing keyboard...")
-	 wm.releaseKeyboard()
-      end
+      keyPressed[key] = true;
 end)
 
 wm.onKeyUp(function (win, mods, key)
+      --print(key .. " released")
       keyPressed[key] = nil;
-      print(key .. mods .. " up")
+
+      if countItems(keyPressed) == 0 and scheduledKbRelease then
+	 wm.releaseKeyboard()
+	 scheduledKbRelease = false
+      end
 end)
 
 print("Success!")
